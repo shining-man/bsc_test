a3887b68d748da8fc9d5a28a634d9a4db227fddb
shining-man <shiningman@mail.de>
2025-04-14 05:18:59 +0200

    test

--- hooks/on_config.py
+++ hooks/on_config.py
@@ -7,8 +7,8 @@ from pathlib import Path
 def gen_blog_redirects(config, path):
     redirects = config["plugins"]["redirects"]
     for md in sorted(path.glob("**/*.md"), reverse=True):
-        if md.name[11:]:
-            redirects.config["redirect_maps"][f"{md.name[11:]}"] = str(md)
+        if md.name[6:]:
+            redirects.config["redirect_maps"][f"{md.name[6:]}"] = str(md)
     return config
 
 
